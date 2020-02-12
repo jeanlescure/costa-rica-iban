5aa660892d65f1892e2479f4e215c4b00cf3db2c
Jean M. Lescure <jeanmlescure@gmail.com>
2020-02-12 15:22:07 -0600

    implement verify iban format test

--- test/criban_test.js
+++ test/criban_test.js
@@ -2,6 +2,7 @@ import CostaRicaIBAN, {
   getCountryPrefixFromIBAN,
   verifyIBANCountryPrefix,
   verifyIBANLength,
+  verifyIBANFormat,
 } from '../src';
 
 const goodIBAN = 'CR06010200009123456789';
@@ -37,6 +38,17 @@ describe('Costa Rica IBAN functions', () => {
       expect(e.message).toBe('Type Error: expected string');
       expect(verifyIBANLength(badIBAN)).toBe(false);
       expect(verifyIBANLength(goodIBAN)).toBe(true);
+    }
+  });
+
+  it('should be able to verify the correct format of the accounts', () => {
+    try {
+      verifyIBANFormat();
+      expect(true).toBe(false);
+    } catch(e) {
+      expect(e.message).toBe('Type Error: expected string');
+      expect(verifyIBANFormat(badIBAN)).toBe(false);
+      expect(verifyIBANFormat(goodIBAN)).toBe(true);
     }
   });
 });
