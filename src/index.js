ada639b8e967083d93b58c363044baecf34d6811
Jean M. Lescure <jeanmlescure@gmail.com>
2020-02-12 17:05:52 -0600

    implement get bank object

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const bankCollection = require('./bank-collection.json');
+const bankCodes = bankCollection.map((b) => b.code);
+
 const NOT_STRING_ERROR = new Error('Type Error: expected string');
+const INVALID_IBAN_ERROR = new Error('Format Error: invalid Costa Rica IBAN format');
 
 const errorIfNotString = (varToverify) => {
   if (typeof varToverify !== 'string') {
@@ -30,15 +34,26 @@ export const verifyIBANFormat = (iban) => {
   return (/^CR\d\d0(1|[3-8])\d{16}$/).test(iban);
 };
 
+const errorOnInvalidIBAN = (iban) => {
+  if (!verifyIBANFormat(iban)) {
+    throw INVALID_IBAN_ERROR;
+  }
+}
+
 export const getBankCodeFromIBAN = (iban) => {
+  errorOnInvalidIBAN(iban);
 
+  return iban.slice(5,8);
 };
 
-export const getBankNameFromBankCode = (bankCode) => {
+export const getBankObjectFromIBAN = (iban) => {
+  const bankCode = getBankCodeFromIBAN(iban);
+  const bankIndex = bankCodes.indexOf(bankCode);
 
+  return bankCollection[bankIndex];
 };
 
-export const getBankNameFromIBAN = (iban) => {
+export const getBankNameFromIBAN = (iban, returnRepresentative = false) => {
 
 };
 
